d766f64ecdd4667be669080596f8e8222addd28f
Richaraj961 <richaraj961@gmail.com>
2018-08-06 15:47:17 +0530

    first commit

---
+++ src/main/java/com/example/seedProject/SeedProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.seedProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeedProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeedProjectApplication.class, args);
+	}
+}
